954337d089b0d21212b9db65bb55d2bbf9d1a622
alakin13@gmail.com <fiiwc8rb>
2022-01-04 00:51:46 +0100

    + add new details to priority Enum

--- src/main/java/Enums/IssuePriorityEnum.java
+++ src/main/java/Enums/IssuePriorityEnum.java
@@ -1,19 +1,24 @@
 package Enums;
 
 public enum IssuePriorityEnum {
-    HIGHEST("Highest"),
-    HIGH("High"),
-    MEDIUM("Medium"),
-    LOW("Low"),
-    LOWEST("Lowest");
+    HIGHEST("Highest", "1"),
+    HIGH("High", "2"),
+    MEDIUM("Medium", "3"),
+    LOW("Low", "4"),
+    LOWEST("Lowest", "5");
 
     private String priorityName;
+    private String priorityId;
 
-    IssuePriorityEnum(String priorityName) {
+    IssuePriorityEnum(String priorityName, String priorityId) {
         this.priorityName = priorityName;
+        this.priorityId = priorityId;
     }
 
     public String getName() {
         return this.priorityName;
+    }
+    public String getId() {
+        return this.priorityId;
     }
 }
