3218c816d2ccf3e57888f4dd40444cc7fb634101
alakin13@gmail.com <fiiwc8rb>
2022-01-04 00:51:58 +0100

    + add new functions to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,10 @@
 import CustomExceptions.JSONNotFoundException;
-import DataParsers.JsonDataProvider;
 import DataParsers.JsonParserInternal;
 import APIConnection.APIConnection;
-import DataParsers.RandomDataGenerator;
+import Enums.IssuePriorityEnum;
 import Enums.IssueTypesEnum;
-import JiraJsonObjects.DataStorageObjects.JiraIssueDataModel;
 import JiraJsonObjects.JiraRequestHandler;
-import JiraJsonObjects.RequestObjects.JiraIssueObject;
 import Utils.Log;
-
-import java.util.Random;
 
 public class Main {
     private static final Log log = new Log(new Object(){}.getClass().getEnclosingClass());
@@ -19,11 +14,66 @@ public class Main {
     private static final IssueTypesEnum typeOfIssue = IssueTypesEnum.TASK;
 
     public static void main(String[] args) throws JSONNotFoundException {
+        deleteIssue();
+    }
+
+    public static void createNewIssue() throws JSONNotFoundException {
         //1. Prepare JSON
-        JiraRequestHandler.createJiraIssueJson(typeOfIssue, dataStoragePath);
+        JiraRequestHandler.createJiraNewIssueJson(typeOfIssue, dataStoragePath);
         //2. Send JSON
-        String response = APIConnection.sendPostRequestWithPayload();
+        String response = APIConnection.sendPostRequestForIssueCreationWithPayload();
         //3. Save response
         JsonParserInternal.saveStringAsJson(response, dataStoragePath + "Output\\JiraAPIResponse01.json");
     }
+
+    public static void editSummaryOfIssue() throws JSONNotFoundException {
+        String issueId = "PIMK-28";
+
+        //1. Prepare JSON
+        JiraRequestHandler.createJiraEditSummaryOfIssueJson(issueId, typeOfIssue, dataStoragePath);
+        //2. Send JSON
+        String response = APIConnection.sendPutRequestForSummaryEditWithPayload(issueId);
+        //3. Save response
+        JsonParserInternal.saveStringAsJson(response, dataStoragePath + "Output\\JiraAPIResponse01.json");
+    }
+
+    public static void editDescriptionOfIssue() throws JSONNotFoundException {
+        String issueId = "PIMK-28";
+
+        //1. Prepare JSON
+        JiraRequestHandler.createJiraEditDescriptionOfIssueJson(issueId, dataStoragePath);
+        //2. Send JSON
+        String response = APIConnection.sendPutRequestForDescriptionEditWithPayload(issueId);
+        //3. Save response
+        JsonParserInternal.saveStringAsJson(response, dataStoragePath + "Output\\JiraAPIResponse01.json");
+    }
+
+    public static void editPriorityOfIssue() throws JSONNotFoundException {
+        String issueId = "PIMK-28";
+        IssuePriorityEnum newPriority = IssuePriorityEnum.HIGHEST;
+
+        //1. Prepare JSON
+        JiraRequestHandler.createJiraEditPriorityOfIssueJson(issueId, newPriority, dataStoragePath);
+        //2. Send JSON
+        String response = APIConnection.sendPutRequestForPriorityEditWithPayload(issueId);
+        //3. Save response
+        JsonParserInternal.saveStringAsJson(response, dataStoragePath + "Output\\JiraAPIResponse01.json");
+    }
+
+    public static void editProjectDetails() {
+        //2. Send JSON
+        String response = APIConnection.sendPostRequestForProjectEditWithoutPayload();
+        //3. Save response
+//        JsonParserInternal.saveStringAsJson(response, dataStoragePath + "Output\\JiraAPIResponse01.json");
+    }
+
+    public static void deleteIssue() throws JSONNotFoundException {
+        String issueId = "PIMK-37";
+
+        //2. Send JSON
+        String response = APIConnection.sendDeleteRequestToDeleteIssueWithPayload(issueId);
+        //3. Save response
+        JsonParserInternal.saveStringAsJson(response, dataStoragePath + "Output\\JiraAPIResponse01.json");
+    }
+
 }
